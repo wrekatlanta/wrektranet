4dee8e67d1ece361b2d2a02daecb26edff6ae184
Patrick Stoica <pstoica@gatech.edu>
2013-08-21 19:37:23 -0400

    admin user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,16 @@ YAML.load(ENV['ROLES']).each do |role|
   Role.find_or_create_by_name(role, :without_protection => true)
   puts 'role: ' << role
 end
-#puts 'DEFAULT USERS'
-#user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-#puts 'user: ' << user.name
-#user.add_role :admin
+
+puts 'DEFAULT USERS'
+user = User.find_by(email: 'gpb@wrek.org') || User.new
+user.email = 'gpb@wrek.org'
+user.first_name = 'George'
+user.last_name = 'Burdell'
+user.username = 'gpb'
+user.password = 'password'
+user.password_confirmation = 'password'
+user.admin = true
+user.save
+
+puts 'user: ' << user.name
