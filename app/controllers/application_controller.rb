c82f9655d3a5265375400093b10d0eefe0c0f40d
Patrick Stoica <pstoica@gatech.edu>
2013-08-22 13:19:29 -0400

    change default flash type, add warning type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  add_flash_types :error, :success, :info
+  add_flash_types :error, :success, :info, :warning
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
