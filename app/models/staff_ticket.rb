4ad3bdbc5af9380af3a9a11fa95b62f253aff3e8
Patrick Stoica <pstoica@gatech.edu>
2013-08-30 16:38:15 -0400

    staff counter cache for contest now only does awarded

--- app/models/staff_ticket.rb
+++ app/models/staff_ticket.rb
@@ -12,8 +12,11 @@
 #
 
 class StaffTicket < ActiveRecord::Base
+  after_save :update_counter_cache
+  after_destroy :update_counter_cache
+
   belongs_to :user
-  belongs_to :contest, -> { includes([:venue]) }, validate: true, counter_cache: :staff_count
+  belongs_to :contest, -> { includes([:venue]) }, validate: true
   belongs_to :contest_director, class_name: "User"
 
   default_scope ->{ order('created_at DESC') }
@@ -47,5 +50,10 @@ class StaffTicket < ActiveRecord::Base
       if self.contest.staff_tickets(:reload).size >= self.contest.staff_ticket_limit
         errors.add(:base, "Too many staff tickets")
       end
+    end
+
+    def update_counter_cache
+      self.contest.staff_count = StaffTicket.awarded.count
+      self.contest.save
     end
 end
