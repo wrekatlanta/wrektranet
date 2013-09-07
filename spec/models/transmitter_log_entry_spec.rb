3dc418c361c0f8b5482dbdf457be652ce383c091
Kyle Petrovich <kylepetrovich@gmail.com>
2013-09-06 23:07:06 -0400

    test

--- spec/models/transmitter_log_entry_spec.rb
+++ spec/models/transmitter_log_entry_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe TransmitterLogEntry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:transmitter_log_entry).should be_valid
+  end
 end
