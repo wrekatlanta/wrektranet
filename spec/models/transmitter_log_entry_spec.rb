02301746342173bfa14fb2332c1462617f478f89
Kyle Petrovich <kylepetrovich@gmail.com>
2013-09-06 23:19:48 -0400

    final test?

--- spec/models/transmitter_log_entry_spec.rb
+++ spec/models/transmitter_log_entry_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe TransmitterLogEntry do
-  it "has a valid factory" do
+  it "has a valid factor" do
     FactoryGirl.create(:transmitter_log_entry).should be_valid
   end
 end
