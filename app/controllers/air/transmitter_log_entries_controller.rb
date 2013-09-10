daf7e3aa387770de5002d193c8edf9ed0a66a019
Kyle Petrovich <kylepetrovich@gmail.com>
2013-09-10 17:44:12 -0400

    inlined form for signing tlog

--- app/controllers/air/transmitter_log_entries_controller.rb
+++ app/controllers/air/transmitter_log_entries_controller.rb
@@ -6,7 +6,24 @@ class Air::TransmitterLogEntriesController < Air::BaseController
   end
 
   def create
-    
+    @tlog = TransmitterLogEntry.new()
+    @tlog.user = current_user
+    @tlog.sign_in = Time.zone.now
+    @tlog.automation_in = transmitter_log_entry_params[:automation_in]
+
+    if @tlog.save
+      redirect_to air_transmitter_log_entries_path, success: "You have signed in."
+    else
+      redirect_to air_transmitter_log_entries_path, error: "Something has gone awry."
+    end
   end
+
+
+  private
+    def transmitter_log_entry_params
+      params.require(:transmitter_log_entry).permit(
+       :automation_in, :automation_out, :sign_out
+      )
+    end
 
 end
