3496d213c5c16aad91db9d184c05336b098221f1
Kyle Petrovich <kylepetrovich@gmail.com>
2013-09-10 17:02:24 -0400

    transmitter log index

--- app/models/transmitter_log_entry.rb
+++ app/models/transmitter_log_entry.rb
@@ -3,7 +3,6 @@ class TransmitterLogEntry < ActiveRecord::Base
 
   validates :user, presence: true
   validates :sign_in, presence: true
-  validates :sign_out, presence: true
 
   scope :today, ->{ where("sign_in > ? and sign_out < ?", Time.zone.now.beginning_of_day, Time.zone.now.end_of_day) }
 
