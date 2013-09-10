3e7f011aab4b0eb12acc523e7949394ae93f3b96
Kyle Petrovich <kylepetrovich@gmail.com>
2013-09-10 17:28:00 -0400

    seperated automation checks, added default

--- app/models/transmitter_log_entry.rb
+++ app/models/transmitter_log_entry.rb
@@ -4,6 +4,6 @@ class TransmitterLogEntry < ActiveRecord::Base
   validates :user, presence: true
   validates :sign_in, presence: true
 
-  scope :today, ->{ where("sign_in > ? and sign_out < ?", Time.zone.now.beginning_of_day, Time.zone.now.end_of_day) }
+  scope :today, ->{ where("sign_in > ? and (sign_out < ? OR sign_out is NULL)", Time.zone.now.beginning_of_day, Time.zone.now.end_of_day) }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130905032404) do
+ActiveRecord::Schema.define(version: 20130910211557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,9 +110,10 @@ ActiveRecord::Schema.define(version: 20130905032404) do
     t.datetime "sign_in"
     t.datetime "sign_out"
     t.integer  "user_id"
-    t.boolean  "automation"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "automation_in",  default: false
+    t.boolean  "automation_out", default: false
   end
 
   create_table "users", force: true do |t|
