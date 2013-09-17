67d6083564ba91c8a0beabf0c0a28048ea7af0f0
Patrick Stoica <pstoica@gatech.edu>
2013-09-17 14:37:16 -0400

    create event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913030655) do
+ActiveRecord::Schema.define(version: 20130917183652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,20 @@ ActiveRecord::Schema.define(version: 20130913030655) do
 
   add_index "contests", ["alternate_recipient_id"], name: "index_contests_on_alternate_recipient_id", using: :btree
   add_index "contests", ["venue_id"], name: "index_contests_on_venue_id", using: :btree
+
+  create_table "events", force: true do |t|
+    t.integer  "eventable_id"
+    t.string   "eventable_type"
+    t.string   "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.boolean  "all_day",        default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "public",         default: true
+  end
+
+  add_index "events", ["eventable_id", "eventable_type"], name: "index_events_on_eventable_id_and_eventable_type", using: :btree
 
   create_table "listener_tickets", force: true do |t|
     t.string   "name"
