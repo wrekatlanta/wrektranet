8f52bad77716b37217ab63d9f98d35d8d929c354
Patrick Stoica <pstoica@gatech.edu>
2013-10-06 16:36:01 -0400

    remove LDAP check from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,13 +33,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :registerable, :recoverable, :rememberable, :trackable, :validatable
-
-  if YAML.load(ENV["USE_LDAP"])
-    devise :devise_ldap_authenticatable
-  else
-    devise :database_authenticatable
-  end
+  devise :registerable, :recoverable, :rememberable, :trackable,
+    :validatable, :database_authenticatable
 
   has_many :staff_tickets, dependent: :destroy
   has_many :contests, through: :staff_tickets
