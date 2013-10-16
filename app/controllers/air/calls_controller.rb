324c7ca43f36f3fd5dd22578fc81cf87e33a8a65
David Garcia <dgarcia32@gatech.edu>
2013-10-16 20:44:43 +0000

    Added pagination for recent calls.

--- app/controllers/air/calls_controller.rb
+++ app/controllers/air/calls_controller.rb
@@ -2,7 +2,8 @@ class Air::CallsController < Air::BaseController
   load_and_authorize_resource except: [:create]
 
   def index
-    @calls = @calls
+    @calls = @calls.paginate(:page => params[:page], :per_page => 30)
+
 
   end
 
