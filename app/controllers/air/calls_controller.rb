5f1010ff3f7bf3a30155e57ae3d130649a3ab93b
David Garcia <dgarcia32@gatech.edu>
2013-10-17 03:04:31 +0000

    Moved the pagination field down.

--- app/controllers/air/calls_controller.rb
+++ app/controllers/air/calls_controller.rb
@@ -1,6 +1,7 @@
 class Air::CallsController < Air::BaseController
   load_and_authorize_resource except: [:create]
 
+
   def index
     @calls = @calls.paginate(:page => params[:page], :per_page => 30)
 
