934e96bef66fb9425d16ed40b23bf12286e2d1a5
Patrick Stoica <pstoica@gatech.edu>
2013-11-05 18:59:32 -0500

    set from field for user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,14 @@
 class UserMailer < ActionMailer::Base
-  after_filter :set_delivery_options
+  default from: "it.director@wrek.org"
 
   def import_email(user, password, email)
     @user = user
     @password = password
+    delivery_options = { user_name: ENV['GMAIL_IT_USERNAME'],
+                         password: ENV['GMAIL_IT_PASSWORD'] }
+
+    mail.delivery_method.settings.merge!(delivery_options)
 
     mail(to: email, subject: "[WREK] Your New Google Apps Account")
   end
-
-  private
-    def set_delivery_options
-      delivery_options = { user_name: ENV['GMAIL_IT_USERNAME'],
-                           password: ENV['GMAIL_IT_PASSWORD'] }
-      mail.delivery_method.settings.merge!(delivery_options)
-    end
 end
