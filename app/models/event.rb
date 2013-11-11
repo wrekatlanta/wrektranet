d099acec9d4a3ae08bdba8799697f1354ce75f05
Patrick Stoica <pstoica@gatech.edu>
2013-11-11 15:10:58 -0500

    pull chronic string getter/setter creation out into NaturalLanguageDate concern

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,9 +16,14 @@
 #
 
 class Event < ActiveRecord::Base
+  include NaturalLanguageDate
+
   after_create :add_to_calendar!
 
   belongs_to :eventable, polymorphic: true
+
+  natural_language_date_attr :start_time
+  natural_language_date_attr :end_time
 
   validates :name, presence: true
   validates :start_time, presence: true
@@ -27,24 +32,6 @@ class Event < ActiveRecord::Base
 
   def end_time_with_fallback(delta = 3.hours)
     self.end_time || (self.start_time ? self.start_time + 3.hours : nil)
-  end
-
-  def start_time_string
-    @start_time_string || self.try(:start_time).try(:strftime, "%-m/%-d/%y %-l:%M %p")
-  end
-
-  def start_time_string=(value)
-    @start_time_string = value
-    self.start_time = parse_date(value)
-  end
-
-  def end_time_string
-    @end_time_string || end_time.try(:strftime, "%-m/%-d/%y %-l:%M %p")
-  end
-
-  def end_time_string=(value)
-    @end_time_string = value
-    self.end_time = parse_date(value)
   end
 
   def add_to_calendar!
@@ -70,17 +57,4 @@ class Event < ActiveRecord::Base
       self.save!
     end
   end
-
-  private
-    def start_time_string_is_date
-      errors.add(:start_time_string, "is invalid") unless parse_date(start_time_string)
-    end
-
-    def end_time_string_is_date
-      errors.add(:end_time_string, "is invalid") unless parse_date(end_time_string)
-    end
-
-    def parse_date(chronic_string)
-      Chronic.parse(chronic_string)
-    end
 end
--- app/models/psa.rb
+++ app/models/psa.rb
@@ -12,47 +12,29 @@
 #
 
 class Psa < ActiveRecord::Base
+  include NaturalLanguageDate
 
-	STATUSES = ['new', 'approved', 'rejected']
+  STATUSES = ['new', 'approved', 'rejected']
 
-	has_many :psa_readings, -> { order('created_at DESC') }
+  has_many :psa_readings, -> { order('created_at DESC') }
+
+  natural_language_date_attr :expiration_date
 
   validate :expiration_date_string_is_date
   validate :bad_expiration_date, on: :create
 
-	validates :title, presence: true
-	validates :body, presence: true
-	validates :expiration_date, presence: true
-	validates :status, inclusion: { in: STATUSES }
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :expiration_date, presence: true
+  validates :status, inclusion: { in: STATUSES }
 
-
-	scope :unexpired, -> { where("expiration_date >= ?", Time.zone.today) }
-	scope :expired, -> { where("expiration_date < ?", Time.zone.today) }
+  scope :unexpired, -> { where("expiration_date >= ?", Time.zone.today) }
+  scope :expired, -> { where("expiration_date < ?", Time.zone.today) }
   scope :approved, -> { includes(:psa_readings).where(status: 'approved') }
   scope :order_by_read, -> { includes(:psa_readings).order('psa_readings.created_at DESC NULLS LAST') }
 
   def bad_expiration_date
-    errors.add(:expiration_date, "Cannot be in the past.") if
+    errors.add(:expiration_date, "cannot be in the past.") if
       !expiration_date.blank? and expiration_date < Time.zone.today
   end
-
-  def expiration_date_string
-    @expiration_date_string || self.try(:expiration_date).try(:strftime, "%-m/%-d/%y")
-  end
-
-  def expiration_date_string=(value)
-    @expiration_date_string = value
-    self.expiration_date = parse_date(value)
-  end
-
-  private
-    def expiration_date_string_is_date
-      errors.add(:expiration_date_string, "is invalid") unless parse_date(expiration_date)
-    end
-
-    def parse_date(chronic_string)
-      Chronic.parse(chronic_string)
-    end
-
-
 end
