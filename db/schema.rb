edccbee0e5c44ec78effad685de6818ea00aa5f7
Patrick Stoica <pstoica@gatech.edu>
2013-11-16 13:54:51 -0500

    try running remember token migration again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113195824) do
+ActiveRecord::Schema.define(version: 20131115015918) do
 
   create_table "contacts", force: true do |t|
     t.string   "email"
@@ -185,6 +185,7 @@ ActiveRecord::Schema.define(version: 20131113195824) do
     t.integer  "buzzcard_id"
     t.integer  "buzzcard_facility_code"
     t.integer  "legacy_id"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
