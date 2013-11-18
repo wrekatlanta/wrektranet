d4a17143e7550df51d70ce1d5fa94faa23a12859
Patrick Stoica <pstoica@gatech.edu>
2013-11-18 13:59:41 -0500

    derp

--- app/models/ldap_helper.rb
+++ app/models/ldap_helper.rb
@@ -5,11 +5,16 @@ module LdapHelper
     @@ldap_conf ||= YAML::load(open("#{Rails.root}/config/ldap.yml"))["production"].symbolize_keys
 
     # Translate admin fields and encryption to Net-LDAP fields
-    @@ldap_conf[:auth] = {method: :simple, username: ldap_conf[:admin_user], password: ldap_conf[:admin_password]}
-    @@ldap_conf[:encryption] = ldap_conf[:ssl] ? {method: :simple_tls} : nil
+    @@ldap_conf[:auth] = {
+      method: :simple,
+      username: @@ldap_conf[:admin_user],
+      password: @@ldap_conf[:admin_password]
+    }
+
+    @@ldap_conf[:encryption] = @@ldap_conf[:ssl] ? {method: :simple_tls} : nil
 
     # Connect with LDAP
-    @@ldap_handle ||= Net::LDAP.new(ldap_conf)
+    @@ldap_handle ||= Net::LDAP.new(@@ldap_conf)
 
     return @@ldap_handle
   end
