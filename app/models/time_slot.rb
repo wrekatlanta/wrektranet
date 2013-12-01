9aae8f981becb0e8d649cc345a2f30380592be05
David Garcia <dgarcia32@gatech.edu>
2013-12-01 16:53:07 +0000

    Annotated time_slot.rb

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -3,19 +3,20 @@
 # Table name: time_slots
 #
 #  id         :integer          not null, primary key
-#  start_time :datetime
-#  end_time   :datetime
+#  start_time :time
+#  end_time   :time
 #  created_at :datetime
 #  updated_at :datetime
 #  user_id    :integer
 #  reason     :string(255)
+#  date       :date
 #
 
 class TimeSlot < ActiveRecord::Base
   validates :user_id, :start_time, :end_time, presence: true
   validates_uniqueness_of :user_id 
   validate :start_time_cannot_be_in_the_past, :end_time_cannot_be_before_start
-
+  scope :today, ->{ where("sign_in > ? and (sign_out < ? OR sign_out is NULL)", Time.zone.now.beginning_of_day, Time.zone.now.end_of_day) }
   belongs_to :user
   def start_time_cannot_be_in_the_past
       if start_time.present? && start_time < Date.today
