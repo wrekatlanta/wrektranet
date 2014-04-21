0d13ecdef985ab7139689547885921419a0a0d45
Patrick Stoica <pstoica@gatech.edu>
2014-04-21 19:50:02 -0400

    ughhh

--- db/migrate/20140418182825_add_exec_staff_to_user.rb
+++ db/migrate/20140418182825_add_exec_staff_to_user.rb
@@ -2,14 +2,14 @@ class AddExecStaffToUser < ActiveRecord::Migration
   def up
     #add_column :users, :exec_staff, :boolean, default: false
 
-    User.with_role(:exec).each do |user|
-      user.exec_staff = true
-      user.save!
-    end
+    # User.with_role(:exec).each do |user|
+    #   user.exec_staff = true
+    #   user.save!
+    # end
 
-    r = Role.find_by(name: "exec")
+    # r = Role.find_by(name: "exec")
 
-    r.destroy! unless r.blank?
+    # r.destroy! unless r.blank?
   end
 
   def down
