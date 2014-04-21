7ae61e9c7aafaeb4ee0bf3aaaa728db54a0fac67
Patrick Stoica <pstoica@gatech.edu>
2014-04-21 19:41:44 -0400

    merp

--- db/migrate/20140418182825_add_exec_staff_to_user.rb
+++ db/migrate/20140418182825_add_exec_staff_to_user.rb
@@ -3,7 +3,6 @@ class AddExecStaffToUser < ActiveRecord::Migration
     add_column :users, :exec_staff, :boolean, default: false
 
     User.with_role(:exec).each do |user|
-      user.remove_role :exec
       user.exec_staff = true
       user.save!
     end
