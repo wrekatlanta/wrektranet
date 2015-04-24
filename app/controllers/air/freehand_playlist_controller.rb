cbc11dc990e5a278c8d42b216ddeab9a59903ae3
Matt Townsend <mtownsend@gatech.edu>
2015-04-24 08:59:27 -0400

    updated freehand play log config

--- app/controllers/air/freehand_playlist_controller.rb
+++ app/controllers/air/freehand_playlist_controller.rb
@@ -3,7 +3,7 @@ class Air::FreehandPlaylistController < Air::BaseController
     @shows = Legacy::Show.specialty_shows
     day = Time.zone.now.strftime("%A")[0..1].downcase
     hour = Time.new(1970,1,1,Time.zone.now.hour)
-    hour = Time.new(1970,1,1,12)
+    #hour = Time.new(1970,1,1,12)
     current = @shows.where(id: Legacy::ShowSchedule.select("show_id").where(days: day).where("start_time <= ?", hour).where("end_time > ?", hour)).first
     if current.nil?
       @current_show = ""
